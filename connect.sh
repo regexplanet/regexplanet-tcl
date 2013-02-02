bb453928804e8d7b9ce93c1d93f1f846c4ca53e9
Andrew M <fileformat@gmail.com>
2013-02-02 10:32:49 -0500

    deploy scripts for NearlyFreeSpeech

---
+++ connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+#
+ssh -i /etc/fileformatnet/nfsnet.pem fileformat_regexplanet-tcl@ssh.phx.nearlyfreespeech.net
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+#
+scp -i /etc/fileformatnet/nfsnet.pem www/* fileformat_regexplanet-tcl@ssh.phx.nearlyfreespeech.net:/home/public
